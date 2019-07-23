20e7ff65c9d38d744e18780781e44d492170fbe1
Matt Daly <mattdaly1235@gmail.com>
2019-07-23 10:18:14 -0400

    install fixes

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 mkdir ~/.vim ~/.vim/bundle
-cp ~/dotfiles/.vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim
+cp -r ~/dotfiles/.vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim
 cp -r ~/dotfiles/.vim/colors/ ~/.vim/colors/
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/liquidprompt/liquidprompt ~/.liquidprompt
