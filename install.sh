3c93f74a508f0b27b253439e5ebfcbe69d3d1204
Matt Daly <mattdaly1235@gmail.com>
2019-07-23 10:51:59 -0400

    now copying full liquidprompt exec

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp -r ~/dotfiles/colors/ ~/.vim/colors/
 ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/liquidprompt/liquidprompt ~/.liquidprompt
+cp ~/dotfiles/liquidprompt/liquidprompt ~/.liquidprompt
 
 vim +PluginInstall +qall
 source ~/.bashrc
